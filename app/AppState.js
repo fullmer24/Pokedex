39238fd1b406b2018cd8eada2c89b15d7414bf04
kade fullmer <fullmer24@gmail.com>
2022-08-03 14:59:21 -0600

    Models started

--- app/AppState.js
+++ app/AppState.js
@@ -2,8 +2,18 @@ import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
 class AppState extends EventEmitter {
-  /** @type {import('./Models/Value').Value[]} */
-  values = []
+  /** @type {import('./Models/Wild.js').Wild[]} */
+  wild = []
+
+  /** @type {import('./Models/Caught.js').Caught[]} */
+  caught = []
+
+  /** @type {import('./Models/Active.js').Active[]} */
+  // @ts-ignore
+  active = {}
+
+
+
 }
 
 export const ProxyState = new Proxy(new AppState(), {
---
+++ app/Models/Wild.js
@@ -0,0 +1,5 @@
+export class Wild {
+    constructor(data) {
+
+    }
+}
